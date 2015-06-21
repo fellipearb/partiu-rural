a2797ca66f15cb0332411f1de771aa7f16ac8e5a
Fellipe Augusto <fellipe.barros@addialeto.net>
2015-06-21 15:06:28 -0300

    Inicio estruturacao

--- Gruntfile.js
+++ Gruntfile.js
@@ -29,7 +29,7 @@ module.exports = function(grunt) {
             dist: {
                 files: [
                     'assets/_js/**/*',
-                    'assets/_sass/**/*'
+                    'assets/_less/**/*'
                 ],
 
                 tasks: ['uglify', 'less']
