f5908090919f7aeee305d50334c647c45114f22e
Fellipe Barros <fellipe1609@gmail.com>
2015-07-15 00:08:45 -0300

    Adicionado site no cake

--- ruraltour.sql
+++ ruraltour.sql
@@ -1,4 +1,4 @@
-CREATE TABLE rt_usuarios(
+CREATE TABLE pr_usuarios(
 	id INT(10) AUTO_INCREMENT PRIMARY KEY,
 	vcNome  VARCHAR(200),
 	vcUsuario VARCHAR(100),
@@ -6,7 +6,7 @@ CREATE TABLE rt_usuarios(
 	blStatus BOOL
 );
 
-CREATE TABLE rt_clientes(
+CREATE TABLE pr_clientes(
 	id INT(100) AUTO_INCREMENT PRIMARY KEY,
 	vcNome VARCHAR(100),
 	vcEmail VARCHAR(100),
@@ -22,37 +22,31 @@ CREATE TABLE rt_clientes(
 	blStatus BOOL
 );
 
-CREATE TABLE rt_anuciante(
+CREATE TABLE pr_anuciante(
 	id INT(100) AUTO_INCREMENT PRIMARY KEY,
 	vcNome VARCHAR(100),
-	vcEmail VARCHAR(100),
-	vcSenha VARCHAR(200),
-	vcCep VARCHAR(9),
 	vcRua VARCHAR(100),
 	idNumero INT(10),
-	vcBairro VARCHAR(80),
-	vcCidade VARCHAR(80),
+	vcComplemento VARCHAR(100),
+	vcBairro VARCHAR(100),
+	vcMunicipio VARCHAR(100),
 	vcEstado VARCHAR(2),
-	vcCpf VARCHAR(14),
-	vcRg VARCHAR(12),
+	vcCep VARCHAR(20),
+	vcCaixaPostal VARCHAR(100),
+	vcCnpj VARCHAR(100),
+	vcInscricaoEstadual VARCHAR(100),
+	vcFoneComercial VARCHAR(20),
+	vcCelular VARCHAR(20),
+	vcEmail VARCHAR(200),
+	vcSite VARCHAR(200),
+	vcBlog VARCHAR(200),
+	vcFacebook VARCHAR(100),
+	vcInstagran VARCHAR(100),
+	vcNomeResponsavel VARCHAR(200),
 	blStatus BOOL
 );
 
-CREATE TABLE rt_empresas(
-	id INT(100) AUTO_INCREMENT PRIMARY KEY,
-	idAnuciante INT(10),
-	vcNomeEmpresa VARCHAR(100),
-	vcCnpj VARCHAR(18),
-	vcCep VARCHAR(9),
-	vcRua VARCHAR(100),
-	idNumero INT(10),
-	vcBairro VARCHAR(80),
-	vcCidade VARCHAR(80),
-	vcEstado VARCHAR(2),
-	blStatus BOOL
-);
-
-CREATE TABLE rt_departamento(
+CREATE TABLE pr_departamento(
 	id INT(10) AUTO_INCREMENT PRIMARY KEY,	
 	vcTitulo VARCHAR(40),
 	txtDescriacao TEXT,
@@ -60,7 +54,7 @@ CREATE TABLE rt_departamento(
 	blStatus BOOL
 );
 
-CREATE TABLE rt_categoria(
+CREATE TABLE pr_categoria(
 	id INT(10) AUTO_INCREMENT PRIMARY KEY,
 	idDepartamento INT(10),
 	vcTitulo VARCHAR(40),
@@ -69,7 +63,7 @@ CREATE TABLE rt_categoria(
 	blStatus BOOL
 );
 
-CREATE TABLE rt_subCategoria(
+CREATE TABLE pr_subCategoria(
 	id INT(10) AUTO_INCREMENT PRIMARY KEY,
 	idCategoria INT(10),
 	vcTitulo VARCHAR(40),
@@ -78,32 +72,26 @@ CREATE TABLE rt_subCategoria(
 	blStatus BOOL
 );
 
-CREATE TABLE rt_opcionais(
+CREATE TABLE pr_opcionais(
 	id INT(10) AUTO_INCREMENT PRIMARY KEY,	
 	vcTitulo VARCHAR(40),
 	txtDescriacao TEXT,
 	blStatus BOOL
 );
 
-CREATE TABLE rt_tipoEmpreendimento(
+CREATE TABLE pr_tipoEmpreendimento(
 	id INT(100) AUTO_INCREMENT PRIMARY KEY,
 	vcTitulo VARCHAR(200),
 	blStatus BOOL
 );
 
-CREATE TABLE rt_empreendimento(
+CREATE TABLE pr_empreendimento(
 	id INT(100) AUTO_INCREMENT PRIMARY KEY,
-	idEmpresa INT(100),
+	idAnuciante INT(100),
 	idTipo INT(10),
 	vcTitulo VARCHAR(100),
 	vcBreveDescricao VARCHAR(200),
 	txtDescriacao TEXT,
-	vcQtdQuartos INT(2),
-	txtDescricaoQuarto TEXT,
-	vcQtdBanheiro INT(2),
-	txtDescricaoBanheiro TEXT,
-	vcEstadiaMinima VARCHAR(20),
-	vcValorPadrao VARCHAR(100),
 	vcRua VARCHAR(100),
 	idNumero INT(10),
 	vcBairro VARCHAR(80),
@@ -114,29 +102,35 @@ CREATE TABLE rt_empreendimento(
 	blStatus BOOL
 );
 
-CREATE TABLE rt_opcionais_empreendimento(
+CREATE TABLE pr_opcionais_empreendimento(
 	id INT(10) AUTO_INCREMENT PRIMARY KEY,	
 	idEmpreendimento INT(10),
 	idOpcional INT(10)
 );
 
-CREATE TABLE rt_fotosEmpreendimento(
+CREATE TABLE pr_fotosEmpreendimento(
 	id INT(10) AUTO_INCREMENT PRIMARY KEY,
 	txtNomeArquivo TEXT,
 	idOdenacao INT(10),
 	blStatus BOOL
 );
 
-CREATE TABLE rt_planosEmpreendimento(
+CREATE TABLE pr_quartosEmpreendimento(
 	id INT(10) AUTO_INCREMENT PRIMARY KEY,
 	idEmpreendimento INT(10),
-	vcValor VARCHAR(100),
-	dtPlanoDe DATETIME,
-	dtPlanoAte DATETIME,
+	vcEstadiaMinima VARCHAR(20),
 	blStatus BOOL
 );
 
-CREATE TABLE rt_avalicaoEmpreendimento(
+CREATE TABLE pr_valoresQuartos(
+	id INT(10) AUTO_INCREMENT PRIMARY KEY,
+	vcValor VARCHAR(100),
+	vcTipoDiaria VARCHAR(100),
+	dtDe DATETIME,
+	dtAte DATETIME,
+);
+
+CREATE TABLE pr_avalicaoEmpreendimento(
 	id INT(10) AUTO_INCREMENT PRIMARY KEY,
 	idEmpreendimento INT(10),
 	idCliente INT(10),
@@ -145,7 +139,7 @@ CREATE TABLE rt_avalicaoEmpreendimento(
 	txtDescriacao TEXT
 );
 
-CREATE TABLE rt_contatos(
+CREATE TABLE pr_contatos(
 	id INT(200) AUTO_INCREMENT PRIMARY KEY,
 	idEmpreendimento INT(10),
 	idCliente INT(10),
@@ -155,7 +149,7 @@ CREATE TABLE rt_contatos(
 	qtdCriancas VARCHAR(200)
 );
 
-CREATE TABLE rt_locacao(
+CREATE TABLE pr_locacao(
 	id INT(200) AUTO_INCREMENT PRIMARY KEY,
 	idEmpreendimento INT(10),
 	idCliente INT(10),
